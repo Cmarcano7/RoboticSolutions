43cc56363906f87ef3fc6d95b3731eda966edfed
CMarcano7 <tino93pr@hotmail.com>
2021-02-25 19:40:09 -0500

    Added access to MongodbAtlas

---
+++ server.js
@@ -0,0 +1,34 @@
+const express = require("express");
+
+const mongoose = require("mongoose");
+const routes = require("./routes");
+const app = express();
+const PORT = process.env.PORT || 3000;
+require('dotenv').config();
+
+// Define middleware here
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+// Serve up static assets (usually on heroku)
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("public"));
+}
+// Add routes, both API and view
+app.use(routes);
+
+//URI connection to Atlas Db
+const uri = process.env.ATLAS_URI;
+mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true});
+
+const connection = mongoose.connection;
+connection.once('open', () => {
+  console.log("MongoDB database connection established successfully");
+})
+
+// Connect to the Mongo DB
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/roboticSolutions");
+
+// Start the API server
+app.listen(PORT, function() {
+  console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
+});
