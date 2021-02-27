f5daf82bc0ae2279a84a1ee49c3669155c3b1440
Jeff Clegg <jeffclegg@att.net>
2021-02-27 14:58:24 -0500

    revised code in Optimization file

--- src/components/Processes/Optimization/index.js
+++ src/components/Processes/Optimization/index.js
@@ -1,50 +1,42 @@
 import React from 'react';
 import {} from 'react-router-dom';
-import {Row, Col, Form} from 'react-bootstrap';
+import '../../process.css';
 
 function Optimization() {
     return(
-        <div>
-            <Form>
+            <form id='integration'> 
             <h5 id='Stages'>Optimization</h5>
-                <Form.Group as={Row} controlId='formOPIR'>
-                <Form.Label id='inputLabels'column sm={2}>Optimization Location:</Form.Label>
-                <Col sm={10}>
-                <Form.Control size='sm' type='OPIR' placeholder='' />
-                </Col>
-            </Form.Group>
-            <Form.Group as={Row} controlId='formODIR'>
-                <Form.Label id='inputLabels'column sm={2}>Optimization Date:</Form.Label>
-                <Col sm={10}>
-                <Form.Control size='sm' type='ODIR' placeholder='' />
-                </Col>
-            </Form.Group>
-            <Form.Group as={Row} controlId='LTOIR'>
-                <Form.Label id='inputLabels'column sm={2}>Leoni Technician who performed Optimization:</Form.Label>
-                <Col sm={10}>
-                <Form.Control size='sm' type='LTOIR' placeholder='' />
-                </Col>
-            </Form.Group>
-            <Form.Group as={Row} controlId='formACRYNIR'>
-                <Form.Label id='inputLabels'column sm={2}>Are Changes Required? Y/N:</Form.Label>
-                <Col sm={10}>
-                <Form.Control size='sm' type='ACRYNIR' placeholder='' />
-                </Col>
-            </Form.Group>
-            <Form.Group as={Row} controlId='formDIROIIR'>
-                <Form.Label id='inputLabels'column sm={2}>Date Integrator Resolved Optimization Issues:</Form.Label>
-                <Col sm={10}>
-                <Form.Control size='sm' type='DIROIIR' placeholder='' />
-                </Col>
-            </Form.Group>
-            <Form.Group as={Row} controlId='formDOCIR'>
-                <Form.Label id='inputLabels'column sm={2}>Date Optimization Complete:</Form.Label>
-                <Col sm={10}>
-                <Form.Control size='sm' type='DOCIR' placeholder='' />
-                </Col>
-            </Form.Group>
-        </Form>
-    </div>
+            <div id='oFormGroup'className='formLabel-group'>
+                <label id='formLabel'>
+                Optimization Location:</label>
+                <input type="text" />
+            </div>
+            <div id='oFormGroup'className='formLabel-group'>
+            <label id='oFormLabel'>
+                Optimization Date:</label>
+                <input type="text" />
+            </div>
+            <div id='oFormGroup'className='formLabel-group'>
+            <label id='oFormLabel'>
+                Leoni Technician who performed Optimization:</label>
+                <input type="text" />
+            </div>
+            <div id='oFormGroup'className='formLabel-group'>
+            <label id='oFormLabel'>
+                Are Changes Required? Y/N:</label>
+                <input type="text" />
+            </div>
+            <div id='oFormGroup'className='formLabel-group'>
+            <label id='oFormLabel'>
+                Date Integrator Resolved Optimization Issues:</label>
+                <input type="text" />
+            </div>
+            <div id='oFormGroup'className='formLabel-group'>
+            <label id='oFormLabel'>
+                Date Optimization Complete:</label>
+                <input type="text" />
+            </div>          
+        </form>
     )
 }
 export default Optimization;
