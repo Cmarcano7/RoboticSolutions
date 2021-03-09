1de233213077f36352269bb2226bc30dc94d6a6f
CMarcano7 <tino93pr@hotmail.com>
2021-03-09 10:13:36 -0500

    testing

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const routes = require("./routes");
 const app = express();
+const cors = require("cors");
 const PORT = process.env.PORT || 3001;
 
 require('dotenv').config();
@@ -10,6 +11,7 @@ require('dotenv').config();
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(cors());
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client"));
